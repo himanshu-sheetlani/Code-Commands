12da6dbc3a75588a1767fe870f5efb9357a71a7d
HARSHITAPALIWAL2 <harshita231196@gmail.com>
2024-04-28 15:53:36 +0530

    javascript to card

--- app.js
+++ app.js
@@ -46,3 +46,30 @@ gsap.to("#nav", {
       scrub: 2,
     },
   });
+  gsap.from("#about-us img,#about-us-in", {
+    y: 90,
+    opacity: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#about-us",
+      scroller: "body",
+      // markers:true,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
+  gsap.from(".card", {
+    scale: 0.8,
+    // opacity:0,
+    duration: 1,
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: ".card",
+      scroller: "body",
+      // markers:false,
+      start: "top 70%",
+      end: "top 65%",
+      scrub: 1,
+    },
+  });
